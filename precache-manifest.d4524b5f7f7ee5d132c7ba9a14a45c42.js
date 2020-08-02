f9cd516c1c44214c53b9d8bf0b5ddddf4ac49a47
Jones Quartey <quarteyjonie18@gmail.com>
2020-08-02 17:02:22 +0100

    Updates

---
+++ precache-manifest.d4524b5f7f7ee5d132c7ba9a14a45c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690527ce821f2f847e2cc1710272b89e",
+    "url": "/React_SignUp_Form/index.html"
+  },
+  {
+    "revision": "878b8e346b2325bbc27e",
+    "url": "/React_SignUp_Form/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b333cb8c224d538e6edb",
+    "url": "/React_SignUp_Form/static/css/main.8f7f3157.chunk.css"
+  },
+  {
+    "revision": "878b8e346b2325bbc27e",
+    "url": "/React_SignUp_Form/static/js/2.bc9ed8b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_SignUp_Form/static/js/2.bc9ed8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b333cb8c224d538e6edb",
+    "url": "/React_SignUp_Form/static/js/main.c59e1b7e.chunk.js"
+  },
+  {
+    "revision": "e64619d3084ff06c2a7e",
+    "url": "/React_SignUp_Form/static/js/runtime-main.12f4b608.js"
+  }
+]);
